f75cc1589bf672fa7852f8b63d26e18ad368c485
Jason <jason.brass@dealertrend.com>
2014-08-11 12:34:34 -0700

    woking-new-detail-page

---
+++ valenti-site-changes-3.js
@@ -0,0 +1,39 @@
+if (jQuery('#dealertrend-inventory-api').length){
+	
+	saleclass=saleclass=jQuery('.eagle-saleclass:first').text();		
+
+
+
+//NEW List and Detail
+
+if (jQuery('.eagle-breadcrumbs a:nth-child(2)').text().match(/new/i) != null) {
+	//List
+	if (jQuery('#eagle-listing').length){
+	//add window stickers to .eagle-listing-buttons
+	}
+	 else {	
+	//Detail
+		vin = jQuery('#eagle-vin-number').text();
+		model=jQuery('#eagle-content-headline .eagle-make').text();
+		  switch(model){
+		  	case 'Ford':
+					linkFord =("<a class='ws' target='_blank' href='http://fordlabels.webview.biz/webviewhybrid/WindowSticker.aspx?vin="+vin+"&ref=VL'>Window Sticker</a>");
+					jQuery('#eagle-stock-vin-wrapper').append(linkFord);
+					jQuery('.ws').css({'font-weight':'bold','color':'red'});
+		  	break;
+		  	case 'Chevrolet':
+					linkChevy =("<a class='ws' target='_blank' href='http://www.bobvalentichevy.com/f_WindowSticker?VIN="+vin+"'>Window Sticker</a>");
+					jQuery('#eagle-stock-vin-wrapper').append(linkChevy);
+					jQuery('.ws').css({'font-weight':'bold','color':'red'});
+		  	break;
+		  	}
+
+      	modelRegex=/(chrysler|dodge|ram|jeep)/i;
+    		if (modelRegex.test(model)) {
+    			linkCDJR = ("<a class='ws' target='_blank' href='http://www.chrysler.com/hostd/windowsticker/getWindowStickerPdf.do?vin="+vin+"'>Window Sticker</a>");
+    			jQuery('#eagle-stock-vin-wrapper').append(linkCDJR);
+					jQuery('.ws').css({'font-weight':'bold','color':'red'});
+    		}
+	}
+}
+}
